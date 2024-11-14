fba16d3186a5e2a813567bca10908b48de6f4cb3
DavidJoao <davidsandoval596@gmail.com>
2024-11-13 18:30:38 -0800

    CREATED Backend route for registering

--- pages/api/user/register.js
+++ pages/api/user/register.js
@@ -1,29 +1,26 @@
-const bcrypt = require('bcrypt');
-import { PrismaClient } from '@prisma/client';
+const bcrypt = require("bcrypt")
+import { PrismaClient } from "@prisma/client"
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
-export default async function POST (request, response) {
-    try { 
-        const form = await request.body;
-        const existingEmail = await prisma.user.findFirst({ where: { email: form.email } });
-
-        if (existingEmail) {
-            response.status(500).json({ error: 'El correo electrónico está en uso' })
-        } else {
-            const hash = await bcrypt.hash(form.password, 10);
-            await prisma.user.create({
-                data: {
-                    name: form.name,
-                    email: form.email,
-                    password: hash,
-                    role: "user",
-                }
-            })
-            response.status(200).json({ success: true });
-        }
-    } catch (error) {
-        console.error('Error:', error);
-        response.status(500).json({ error: "Internal Server Error" })
-    }
+export default async function POST(request, response) {
+	try {
+		const form = await request.body
+		const hash = await bcrypt.hash(form.password, 10)
+		await prisma.user.create({
+			data: {
+				name: form.name,
+				email: form.email,
+				password: hash,
+				status: "active",
+				lastActivity: new Date(),
+				lastLogin: new Date(),
+				registerTime: new Date(),
+			},
+		})
+		response.status(200).json({ success: true })
+	} catch (error) {
+		console.error("Error:", error)
+		response.status(500).json({ error: 'Email Already In Use' })
+	}
 }

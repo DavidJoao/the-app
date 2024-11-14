472ca2f0f458b841a77f2984a6f384066348ff2c
DavidJoao <davidsandoval596@gmail.com>
2024-11-13 19:42:46 -0800

    ADDED Redirection to main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,12 @@
 'use client'
-import Link from "next/link"
+import { navigate } from "./lib/redirect"
 
 export default function Home() {
+
+	navigate('/pages/login')
+
 	return (
 		<div className="App">
-			<h1 className="main-title">The App</h1>
-			<div className="w-full md:w-[40%] h-[400px] rounded-lg shadow-xl bg-white">
-				<form className="form">
-					<label>Email:</label>
-					<input className="input" />
-					<label>Password:</label>
-					<input className="input" />
-					<button className="blue-button">Log In</button>
-					<p className="mx-auto">or</p>
-					<Link href={"/pages/signup"} className="underline underline-offset-4 mx-auto">
-						Create an account
-					</Link>
-				</form>
-			</div>
 		</div>
 	)
 }

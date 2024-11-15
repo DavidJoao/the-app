071d59af928a4eaf89d6bffa30ed60803cb693b1
DavidJoao <davidsandoval596@gmail.com>
2024-11-14 22:28:29 -0800

    UPDATED Route for lastlogin

--- pages/api/user/lastLogin/[email].js
+++ pages/api/user/lastLogin/[email].js
@@ -2,14 +2,19 @@ import { prismaClient } from "@/app/lib/prismaClient";
 
 export default async function (req, res) {
     const { email } = req.query;
-    try {
-        const updatedUser = await prismaClient.user.update({ 
-            where: { email: email},
-            data: { lastLogin: new Date() }
-         })
-         res.status(200).json(updatedUser)
-    } catch (error) {
-        console.error("Error updating lastLogin:", error);
-        res.status(500).json({ error: "Unable to update lastLogin" });
+    const foundUser = await prismaClient.user.findUnique({ where: { email: email } })
+    if (foundUser.status === 'blocked') {
+        return res.status(400).json({ error: "Account Blocked" });
+    } else {
+        try {
+            const updatedUser = await prismaClient.user.update({ 
+                where: { email: email},
+                data: { lastLogin: new Date() }
+             })
+             res.status(200).json(updatedUser)
+        } catch (error) {
+            console.error("Error updating lastLogin:", error);
+            res.status(500).json({ error: "Unable to update lastLogin" });
+        }
     }
 }

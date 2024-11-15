5fe72a95c49a35393809b1352088b699d37eb4ad
DavidJoao <davidsandoval596@gmail.com>
2024-11-14 22:31:23 -0800

    CHANGED Form width for mobile devices

--- src/app/pages/signup/page.js
+++ src/app/pages/signup/page.js
@@ -62,7 +62,7 @@ const Signup = () => {
   return (
     <div className='App'>
       <Link href={'/'} className="main-title">The App</Link>
-      <form className='form w-[40%] auto rounded-lg shadow-xl bg-white' onSubmit={handleSubmit}>
+      <form className='form w-[95%] md:w-[40%] auto rounded-lg shadow-xl bg-white' onSubmit={handleSubmit}>
         <label>Email</label>
         <input required name='email' value={form.email} className='input' onChange={handleChange}/>
         <label>Name</label>

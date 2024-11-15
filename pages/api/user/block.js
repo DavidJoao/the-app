aa4b2e69fdadd7770dbd42129197456c6d62268e
DavidJoao <davidsandoval596@gmail.com>
2024-11-14 22:31:39 -0800

    CREATED Backend route for blocking users

---
+++ pages/api/user/block.js
@@ -0,0 +1,15 @@
+import { prismaClient } from "@/app/lib/prismaClient"
+
+export default async function (req, res) {
+    const users = req.body;
+    try {
+        const response = await prismaClient.user.updateMany({ 
+            where: { user_id: { in: users }},
+            data: { status: 'blocked' }
+         })
+         return res.status(200).json({ success: true, response });
+    } catch (error) {
+        console.error({ error: error })
+        return res.status(500).json({ success: false, error: error.message });
+    }
+}

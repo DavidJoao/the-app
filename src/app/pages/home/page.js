f67a38a5e14968ba3364b8f09700d491197fd8b7
DavidJoao <davidsandoval596@gmail.com>
2024-11-14 18:24:57 -0800

    ADDED Table for info, still need to work on table style, blocking, deleting and format dates

--- src/app/pages/home/page.js
+++ src/app/pages/home/page.js
@@ -1,13 +1,55 @@
-'use client'
-import { logoutUser } from '@/app/lib/actions/session'
-import React from 'react'
+"use client"
+import Navbar from "@/app/components/Navbar"
+import React, { useEffect, useState } from "react"
+import useAuth from "@/app/hooks/useAuth"
+import axios from "axios"
 
 const Home = () => {
-  return (
-    <div>
-        <button onClick={() => logoutUser()}>Logout</button>
-    </div>
-  )
+
+	const session = useAuth()
+
+  const [users, setUsers] = useState(null)
+
+  useEffect(() => {
+    axios.get('/api/user/all')
+      .then(res => {
+        setUsers(res?.data?.users)
+      })
+      return
+  }, [])
+
+	return (
+		<div className="bg-slate-200 flex flex-col h-screen">
+			<Navbar user={session?.user}/>
+			<div className="p-3 mt-[50px]">
+				<p className="sub-title">Dashboard</p>
+				<table>
+					<thead>
+						<tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Last Login</th>
+              <th>Last Activity</th>
+						</tr>
+					</thead>
+					<tbody>
+          { users?.map((user, index) => {
+            return (
+              <tr key={index}>
+                    <input type="checkbox"/>
+                    <td>{user?.name}</td>
+                    <td>{user?.email}</td>
+                    <td>{user?.lastLogin}</td>
+                    <td>{user?.lastActivity}</td>
+                 </tr>
+                )
+              })}
+					</tbody>
+				</table>
+			</div>
+		</div>
+	)
 }
 
 export default Home

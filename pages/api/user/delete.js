3b34dc9740fcb66c04914577dbdfb9157756072b
DavidJoao <davidsandoval596@gmail.com>
2024-11-14 22:32:04 -0800

    CREATED Backend routes for deleting, unblocking and updating last activity

---
+++ pages/api/user/delete.js
@@ -0,0 +1,14 @@
+import { prismaClient } from "@/app/lib/prismaClient"
+
+export default async function (req, res) {
+    const users = req.body;
+    try {
+        const response = await prismaClient.user.deleteMany({ 
+            where: { user_id: { in: users }}
+         })
+         return res.status(200).json({ success: true, response });
+    } catch (error) {
+        console.error({ error: error })
+        return res.status(500).json({ success: false, error: error.message });
+    }
+}
---
+++ pages/api/user/lastActivity/[email].js
@@ -0,0 +1,20 @@
+import { prismaClient } from "@/app/lib/prismaClient";
+
+export default async function (req, res) {
+    const { email } = req.query;
+    const foundUser = await prismaClient.user.findUnique({ where: { email: email } })
+    if (foundUser.status === 'blocked') {
+        return res.status(400).json({ error: "Account Blocked" });
+    } else { 
+        try {
+            const updatedUser = await prismaClient.user.update({ 
+                where: { email: email},
+                data: { lastActivity: new Date() }
+             })
+             res.status(200).json(updatedUser)
+        } catch (error) {
+            console.error("Error updating last activity:", error);
+            res.status(500).json({ error: "Unable to update last activity" });
+        }
+    }
+}

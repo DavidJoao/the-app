831869c3b63a6666be854ebd8724d7d76f615128
DavidJoao <davidsandoval596@gmail.com>
2024-11-15 19:50:00 -0800

    CREATED Global Context for notifications when user is blocked/deleted

--- src/app/components/context/provider.js
+++ src/app/components/context/provider.js
@@ -1,12 +1,21 @@
 "use client"
  import { SessionProvider } from 'next-auth/react'
+ import { createContext, useContext, useState } from 'react'
+
+ const AppContext = createContext();
 
  const Provider = ( { children } ) => {
+
+    const [appNotification, setAppNotification] = useState("")
+
     return (
         <SessionProvider>
-            {children}
+            <AppContext.Provider value={{ appNotification, setAppNotification }}>
+                {children}
+            </AppContext.Provider>
         </SessionProvider>
     )
  }
-
  export default Provider
+
+ export const useAppContext = () => useContext(AppContext);
